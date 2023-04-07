f1ef8c15ac51341ac04f7798d07f5c3502790239
yaroslav12103 <130151377+yaroslav12103@users.noreply.github.com>
2023-04-07 15:38:01 +0400

    Add files via upload
    Ля ля ля ля ля ля

---
+++ script.js
@@ -0,0 +1,18 @@
+$('.carousel').slick({
+  	slidesToShow: 1,
+  	slidesToScroll: 1,
+  	autoplay: true,
+ 	autoplaySpeed: 2000,
+ 	prevArrow: '<img src="фотки/leftA.png" width="30px" height="30px" class="arrow">',
+  	nextArrow: '<img src="фотки/rightA.png" width="30px" height="30px" class="arrow">',
+});
+
+let cb = document.querySelector('.content-blocker')
+let burger = document.querySelector('.burger-menu')
+let nav = document.querySelector('.main-nav')
+burger.addEventListener('click', function() {
+	 cb.classList.toggle('active')
+    burger.classList.toggle('active')
+    nav.classList.toggle('active') 
+})
+
